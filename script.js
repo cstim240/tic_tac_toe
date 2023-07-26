4bf0f09232fc47d11ac4778998c07b7f48c884a5
Tim Supan <timsupann@gmail.com>
2023-07-26 12:01:12 -0700

    Adjust playerFactory function to return method and properties

--- script.js
+++ script.js
@@ -14,9 +14,9 @@ const gameBoard = (function() {
 
 
 const playerFactory = function(playerType) {
-    let playerSymbol = '';
+    let playerSymbol;
     
-    let declareSymbol = (playerType) => {
+    function assignSymbol() {
         if (playerType == 'user'){
             playerSymbol = 'X';
         } else {
@@ -24,8 +24,21 @@ const playerFactory = function(playerType) {
         } 
     }
 
-    return declareSymbol;
+    function getPlayerSymbol(){
+        return playerSymbol; //either X or O
+    }
+
+    function getPlayerType(){
+        return playerType; //could be user, or bot, or anything else
+    }
+
+    assignSymbol();
+    return {assignSymbol: assignSymbol,
+        getPlayerSymbol: getPlayerSymbol,
+        getPlayerType: getPlayerType
+    };
 }
 
 const user = playerFactory('user');
-console.log(user.declareSymbol);
+console.log(user.getPlayerSymbol());
+
