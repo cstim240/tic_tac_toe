274ad950b1d46ea3e74e8314bc887c800a5278d5
Tim Supan <timsupann@gmail.com>
2023-07-26 12:23:32 -0700

    Add comments about ES6

--- script.js
+++ script.js
@@ -31,16 +31,19 @@ const playerFactory = function(playerType) {
         },
         getPlayerType(){
             return playerType; //could be user, or bot, or anything else
-    }
         }
-    };
+        }
+};
+    /*assignSymbol is short-hand (ES6), for the more verbose way
+    it would be variable : function, in this case assignSymbol : assignSymbol
+    */
 
     /*
     The object literal, when returned by the playerFactory function, 
     creates a player object with the specified properties and methods. 
     The shorthand notations help keep the code concise and more readable.
     */
-}
+
 
 const user = playerFactory('user');
 console.log(user.getPlayerSymbol());
